554e24d6ef9fafa92222e29d719ac1e3967f3e2a
Bruno Vinícius <xbrunosousa@icloud.com>
2017-02-19 19:09:32 -0300

    Add files

---
+++ js/chama-mapa.js
@@ -0,0 +1,19 @@
+$(function(){	
+	
+	$('#mapa').jmap('init');
+	
+	$('form').submit(function(){
+		
+		$('#mapa').jmap('SearchDirections', {
+			'query': 'from:' + $('#partida').val() + ' to:' + $('#chegada').val(),
+			'locale': 'pt_BR',
+			'panel':'#direcoes'
+		}, function(result) {
+			preco = Math.ceil(result.getDistance().meters /1000 * 1.9 + 4);
+			$('#aviso').html("Preço aproximado: R$" + preco);
+	   });
+	   
+		return false;
+	});
+	
+});
