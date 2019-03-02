4ddebdcbf04d09808f9546188e757de71656970d
Bruno Vinícius <xbrunosousa@icloud.com>
2019-03-01 23:13:04 -0300

    #00 Adjusted components and sass

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,26 @@
-import React, { Component } from 'react';
-import './App.css';
-import Map from './Map/Map';
-import Form from './Form/Form';
-import { Container, Alert } from 'reactstrap';
-import ReactGA from 'react-ga';
-import { directionsDefault } from './Map/defaultDirections';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React, { Component } from "react";
+import Map from "./Map";
+import Form from "./Form";
+import { Container, Alert } from "reactstrap";
+import ReactGA from "react-ga";
+import { directionsDefault } from "../partials/defaultDirections";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/scss/main.scss";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       toastDefaultProps: {
-        position: 'top-right',
+        position: "top-right",
         autoClose: 5000,
         hideProgressBar: false,
         closeOnClick: true,
         pauseOnHover: true,
         draggable: true
       },
-      origin: 'Torre de TV, Brasília',
-      destination: 'Esplanada dos Ministérios',
+      origin: "Torre de TV, Brasília",
+      destination: "Esplanada dos Ministérios",
       isSubmited: false,
       directions: directionsDefault,
       distance: undefined
@@ -30,7 +29,7 @@ class App extends Component {
 
   componentDidMount() {
     // Google Analytics
-    ReactGA.initialize('UA-121994767-1');
+    ReactGA.initialize("UA-121994767-1");
     ReactGA.pageview(window.location.pathname + window.location.search);
   }
 
@@ -72,13 +71,13 @@ class App extends Component {
         } else {
           this.setState({ errorRequisition: true });
           toast.error(
-            'Houve um erro. Verifique o endereço digitado.',
+            "Houve um erro. Verifique o endereço digitado.",
             this.state.toastDefaultProps
           );
         }
         if (this.state.distance === 0) {
           toast.error(
-            'A distância minima é de 1km!',
+            "A distância minima é de 1km!",
             this.state.toastDefaultProps
           );
         }
@@ -96,12 +95,8 @@ class App extends Component {
   };
 
   render() {
-    const keyGmaps = 'AIzaSyBZJDUkG83bcVMgdRoJPOotgt0v305l6W4';
-    const {
-      directions,
-      price,
-      distance,
-    } = this.state;
+    const keyGmaps = "AIzaSyBZJDUkG83bcVMgdRoJPOotgt0v305l6W4";
+    const { directions, price, distance } = this.state;
     return (
       <div className="App">
         <Container fluid>
@@ -115,19 +110,19 @@ class App extends Component {
           <div className="mapa-xbs">
             <Map
               googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${keyGmaps}&v=3.exp&libraries=geometry,drawing,places`}
-              loadingElement={<div style={{ height: '100vh' }} />}
-              containerElement={<div style={{ height: '100vh' }} />}
-              mapElement={<div style={{ height: '100vh' }} />}
+              loadingElement={<div style={{ height: "100vh" }} />}
+              containerElement={<div style={{ height: "100vh" }} />}
+              mapElement={<div style={{ height: "100vh" }} />}
               directions={directions}
             />
           </div>
 
           {distance >= 1 && (
             <Alert className="success-search" color="success">
-              Distância: {distance} km – Valor total:{' '}
-              {price.toLocaleString('pt-br', {
-                style: 'currency',
-                currency: 'BRL'
+              Distância: {distance} km – Valor total:{" "}
+              {price.toLocaleString("pt-br", {
+                style: "currency",
+                currency: "BRL"
               })}
             </Alert>
           )}
