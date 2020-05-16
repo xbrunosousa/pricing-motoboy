658cbbbf1d4a6ddd40316d866f84aa167962b6b5
Bruno Sousa <xbrunosousa@icloud.com>
2020-05-16 13:44:56 -0300

    Updates

---
+++ precache-manifest.fee94e1589140e71536a6eec08c6f2d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79107a77775a8ae46753728b27c833a7",
+    "url": "/pricing-motoboy/index.html"
+  },
+  {
+    "revision": "0b44882755bc745a4b90",
+    "url": "/pricing-motoboy/static/css/2.efd39d59.chunk.css"
+  },
+  {
+    "revision": "8da15f2cee24ac6364be",
+    "url": "/pricing-motoboy/static/css/main.9fd8900d.chunk.css"
+  },
+  {
+    "revision": "0b44882755bc745a4b90",
+    "url": "/pricing-motoboy/static/js/2.c2034657.chunk.js"
+  },
+  {
+    "revision": "6d94984a5fdbad217098935a3d45084e",
+    "url": "/pricing-motoboy/static/js/2.c2034657.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8da15f2cee24ac6364be",
+    "url": "/pricing-motoboy/static/js/main.7dd7c7d4.chunk.js"
+  },
+  {
+    "revision": "082493c05578f5cfa4a4",
+    "url": "/pricing-motoboy/static/js/runtime-main.e0b20f5f.js"
+  }
+]);
